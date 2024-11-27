2939a49f9ecd31111870bc77a62156019c98e939
Raamprathap <144192481+Raamprathap@users.noreply.github.com>
2024-11-27 13:19:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify, send_from_directory, render_template  # Added render_template import
 from flask_cors import CORS
 import os
 from urllib.parse import quote
